5cf6f75442c8b5cacc1e9f19b107be70961cb88d
Janessa Munt <janessamunt@gmail.com>
2022-03-06 00:22:42 -0800

    Comments so future Janessa knows whats going on

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const { API_KEY } = require("./api-key");
 const { Game, convertCoordsToBBoxes } = require("@gathertown/gather-game-client");
 const { setgroups } = require("process");
 global.WebSocket = require("isomorphic-ws");
-
-/**** setup ****/
-
-// what's going on here is better explained in the docs:
-// https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
 const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
-game.connect("jEDZjhqoI4iAKolF\\OrigamiUSA"); // replace with your spaceId of choice
+game.connect("jEDZjhqoI4iAKolF\\OrigamiUSA"); // replace with your spaceId
 game.subscribeToConnection((connected) => console.log("connected?", connected));
+const mapId = "Origami Cafe"; //replace with your map of choice 
 
-//images left at index 0, right at 1, up at 2, down at 3
-//walk left test at 2
+//images left at index 0, right at 1, up at 2, down at 3, then repeats with alternate forms for each direction
+//this part looks gross, someone please tell me how to make it less gross if I wana add the other 3024982049 pokemon
 let pokeball =
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/tY3NKLnSH19EEstnaZ6Ix5";
-let Charmander =
+let Charmander =    
   ["https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/Gs2JPhCJJXGzFEegABNp7g",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/nYQ0cEdRlQdMs6fu7FXVZd",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/Tp75dv75IIAWdanFsPfxWG",
@@ -24,7 +20,6 @@ let Charmander =
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/O4Ulzd5by5VNdbbKDIrFBj",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/1hHwGrACK86OwGZjcrSC2W",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/Rwv9YE7UjV42VFHBnmEvee"
-
 ];
 let Squirtle =  //lots of placeholders
   ["https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ggYDMTlJdsJGyFLA35O5pG", 
@@ -35,23 +30,19 @@ let Squirtle =  //lots of placeholders
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ohkujdHjBLKDcJQP2cWNHj",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ggYDMTlJdsJGyFLA35O5pG", 
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ohkujdHjBLKDcJQP2cWNHj"
-  
 ];
 let Bulbasaur =     //lots of placeholders
   ["https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/d02kXkvXlC2WGgOrjfoO2v",
   "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/1f9c898TFKMl1fM2QSkN3w",
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ggYDMTlJdsJGyFLA35O5pG", 
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ohkujdHjBLKDcJQP2cWNHj",
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ggYDMTlJdsJGyFLA35O5pG", 
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ohkujdHjBLKDcJQP2cWNHj",
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ggYDMTlJdsJGyFLA35O5pG", 
-  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/ohkujdHjBLKDcJQP2cWNHj"
-  
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/d02kXkvXlC2WGgOrjfoO2v",
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/1f9c898TFKMl1fM2QSkN3w",
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/d02kXkvXlC2WGgOrjfoO2v",
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/1f9c898TFKMl1fM2QSkN3w",
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/d02kXkvXlC2WGgOrjfoO2v",
+  "https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/jEDZjhqoI4iAKolF/1f9c898TFKMl1fM2QSkN3w"
 ];
 
-const mapId = "Origami Cafe";
-
-//get new x and y for pokemon based on movement
+//returns new x,y, and direction sprite should be facing for pokemon based on current location/movement
 function getNewPokeLocation(x, y, direction){
   if(direction == 1 || direction ==2 ){     //down
     return [x, y-1, 3];
@@ -67,20 +58,21 @@ function getNewPokeLocation(x, y, direction){
   }
 }
 
+//moves pokemon to follow u, returns nothing
 function movePokemon(context){
   //make object
   let objectId = context.playerId +"Pokemon";
-  let location = getNewPokeLocation(context.player.x, context.player.y, context.player.direction);
-  //console.log(eval(context.player.affiliation));
+  let location = getNewPokeLocation(context.player.x, context.player.y, context.player.direction);  //index 0 is x and 1 is y. 2 is direction
   let state = 'normal';
   let imageLocation = eval(context.player.affiliation)[location[2]];
 
   //check if image should alternate
-  if(game.getObject(objectId).obj.customState == 'normal' && context.player.affiliation == 'Charmander'){   //only charmander has all the sprites, check for that
-    imageLocation = eval(context.player.affiliation)[location[2]+4];
-     state = 'walking'
+  if(game.getObject(objectId) != undefined){    //something something this gets mad at me if this is undefined and i try to move on
+    if(game.getObject(objectId).obj.customState == 'normal' && context.player.affiliation == 'Charmander'){   //only charmander has all the sprites, check for that, remove second half if all your sprites have all 8 images
+      imageLocation = eval(context.player.affiliation)[location[2]+4];
+      state = 'walking'
+    }
   }
-
 
   let obj = {
     id: objectId,
@@ -93,20 +85,18 @@ function movePokemon(context){
     customState: state
   }
 
-
   //add object
   setTimeout(() => {
-  game.setObject('Origami Cafe', objectId, obj);
+  game.setObject(mapId, objectId, obj);
   },200);
 }
 
 function setUp(){
-  let names = ["Charmander", "Squirtle", "Bulbasaur", "Put Away"];     //array with values
-  //starting x and y
+  let names = ["Charmander", "Squirtle", "Bulbasaur", "Put Away"];     //array with values, can add more pokemon if you have loaded in sprites
+
+  //starting x and y for top left pokeball, the rest of the pokeballs will spawn to the right of the first one
   let startX = 27;
   let startY = 20;
-
-  //let objectId = "squirtleBall"; 
 
   setTimeout(() => {
     for(let i=0; i<names.length; i++){
@@ -121,47 +111,33 @@ function setUp(){
         width: 1, 
         height: 1, 
         previewMessage: names[i], // this is what shows up in the press x bubble
-
       };
-
-    console.log("setting the object");
     game.setObject(mapId, objectId, obj);
-  }
-
+    }
   }, 3000); 
 }
 
-
-
-
 function pokemon(){
-  //watch for pokemon choosing
-  game.subscribeToEvent("playerInteracts", (data, context) => {
+  game.subscribeToEvent("playerInteracts", (data, context) => {     //watch for pokemon choosing
     let mapObject = game.getObject(data.playerInteracts.objId).obj; 
     let pokePick = game.getObject(data.playerInteracts.objId).obj.previewMessage;
   
-        //put away pokemon
-  if (pokePick == 'Put Away'){
+    if (pokePick == 'Put Away'){    //put away pokemon
     game.setAffiliation('', context.playerId);
     game.deleteObject(mapId, context.playerId + "Pokemon");
-  }
-  else {
+    }
+    else {
     game.setAffiliation(pokePick, context.playerId);   //set pokemon
-  
-  }
-  
-  });
+    }
+   });
 
   game.subscribeToEvent("playerMoves", (data, context) =>{    //watch for movements
-    if(context.player.affiliation != ''){
-     // console.log(game.getObject(context.playerId + "Pokemon"));
-      //move object
-        movePokemon(context);
+    if(context.player.affiliation != ''){     //only try and set pokmon if player has selected one
+        movePokemon(context); //makes that pokemon follow you!
     }
   });
 
-  //spawn pokemon on entry
-  //despawn pokemon on leave
+  //despawn pokemon on leave, goodbye friend :c
   game.subscribeToEvent("playerExits", (data, context) =>{
     console.log(context);
     game.deleteObject(mapId, context.playerId + "Pokemon");
@@ -169,6 +145,6 @@ function pokemon(){
 
 }
 
-setUp();
+setUp();  //comment out after first run
 pokemon();
 
